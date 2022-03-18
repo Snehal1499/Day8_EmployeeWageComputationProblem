8233208f5b8ff928c045112f825a3b1444d32762
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-18 13:49:36 +0530

    Added UC6 Employee Wage Problem

--- EmployeeWageComputationProblem/Program.cs
+++ EmployeeWageComputationProblem/Program.cs
@@ -8,41 +8,44 @@ namespace EmployeeWageComputationProblem
         public int is_FullTime = 1;
         public int is_PartTime = 2;
         public int emp_Rate_Per_Hr = 20;
-        public int no_of_Working_Days = 20;
+        public int total_Working_Hrs = 99;
+        public int total_Work_Days_In_Month = 20;
 
         //Variables
         public int empHrs = 0;
-        public int empWage_per_day = 0;
-        public int empWage_per_Month = 0;
-
+        public int empTotalWage = 0;
+        public int total_Worked_DAys = 0;
+        public int total_Worked_Hrs = 0;
     }
     public class Program
     {
         public static void Main(string[] args)
         {
             UC5_EmployeeWage UC5 = new UC5_EmployeeWage();
-            Random random = new Random();
-
             //Computation
+            while (UC5.total_Worked_DAys <= UC5.total_Work_Days_In_Month || UC5.total_Worked_Hrs <= UC5.total_Working_Hrs)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                UC5.total_Worked_DAys++;
+                UC5.total_Worked_Hrs++;
+                switch (empCheck)
+                {
+                    case 1:
+                        UC5.empHrs = 8;
+                        break;
+                    case 2:
+                        UC5.empHrs = 4;
+                        break;
+                    default:
+                        UC5.empHrs = 0;
+                        break;
+                }
 
-            int empCheck = random.Next(0, 3);
-            if (empCheck == UC5.is_FullTime)
-            {
-                UC5.empHrs = 8;
-
             }
-            else if (empCheck == UC5.is_PartTime)
-            {
-                UC5.empHrs = 4;
-            }
-            else
-            {
-                UC5.empHrs = 0;
-            }
-            UC5.empWage_per_day = UC5.empHrs * UC5.emp_Rate_Per_Hr;
-            Console.WriteLine("Wage Per Day=" + UC5.empWage_per_day);
-            UC5.empWage_per_Month = UC5.empWage_per_day * UC5.no_of_Working_Days;
-            Console.WriteLine("Wage PEr Month=" + UC5.empWage_per_Month);
+            Console.WriteLine("Total Worked Hr =" + UC5.total_Worked_Hrs);
+            UC5.empTotalWage = UC5.total_Worked_Hrs * UC5.emp_Rate_Per_Hr;
+            Console.WriteLine("emp Total Wage =" + UC5.empTotalWage);
         }
     }
 }
