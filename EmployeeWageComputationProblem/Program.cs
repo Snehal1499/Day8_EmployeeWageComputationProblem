33098923b79cd330fe834b3bf36261b824e62b99
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-18 11:41:55 +0530

    Added Welcome Message

---
+++ EmployeeWageComputationProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageComputationProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+        }
+    }
+}
