2e0078783a7c4ef8defdc23be4d54dc0291a5a29
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-18 12:05:05 +0530

    Added UC1_EmployeeWage

--- EmployeeWageComputationProblem/Program.cs
+++ EmployeeWageComputationProblem/Program.cs
@@ -2,11 +2,32 @@
 
 namespace EmployeeWageComputationProblem
 {
-    internal class Program
+    public class UC1_EmployeeWage
+    {
+        //Constants
+        public int Is_Full_Time = 1;
+    }
+    public class Programe
+   
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
-        }
+            //Computation
+            Random random = new Random();
+            int empcheck = random.Next(0,2);
+            UC1_EmployeeWage emp = new UC1_EmployeeWage ();
+
+            if (empcheck == emp.Is_Full_Time)
+
+            {
+                Console.WriteLine("Employee is Present ");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent ");
+            }
+
+            }
     }
 }
